0a9a15620342a1a495cb6d83fc1346e06859ad64
Leonard Atorough <leotorough@gmail.com>
2026-05-12 15:41:59 +0100

    feat: Implement Pagination component with navigation controls and accessibility features

---
+++ packages/core/src/components/Pagination/index.ts
@@ -0,0 +1,3 @@
+export { Pagination } from "./Pagination";
+export { paginationVariants, paginationButtonVariants, paginationEllipsisVariants } from "./pagination.variants";
+export type { PaginationProps, PageNavigationIcons as PaginationIcons } from "./pagination.types";

0c23b08f88f1e9726d05747e32f55a77668a70d2
Leonard Atorough <leotorough@gmail.com>
2026-05-12 13:48:33 +0100

    feat: add TypeScript support to ESLint configuration and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactPlugin from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import js from "@eslint/js";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  { ignores: ['dist', 'build', 'coverage', 'node_modules', '.next'] },
+  { ignores: ["dist", "build", "coverage", "node_modules", ".next", "storybook-static"] },
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,11 +18,33 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
-      'react-hooks/exhaustive-deps': 'warn',
+      "react-hooks/exhaustive-deps": "warn",
     },
     plugins: {
-      'react-hooks': reactPlugin,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactPlugin,
+      "react-refresh": reactRefresh,
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: true,
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "react-hooks": reactPlugin,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
